ce164de427c711692abbbcacaba7d426467ff44a
Elena Mountzia <el.mountzia@gmail.com>
2021-05-07 01:22:42 +0300

    Add song model

---
+++ src/data/infrastructure/db/models/song.js
@@ -0,0 +1,33 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('..');
+
+const Song = db.define('songs', {
+  id: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  artist: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  album: {
+    type: Sequelize.STRING
+  },
+  coverUrl: {
+    type: Sequelize.STRING
+  },
+  track: {
+    type: Sequelize.BLOB('long'),
+    allowNull: false
+  }
+}, {
+  
+});
+
+module.exports = Song;
